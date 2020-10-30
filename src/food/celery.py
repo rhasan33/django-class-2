d2d30ba27fd4d35ba6aec90eb65f5c95d452ed8a
rhasan33 <rakib.hasan@evaly.com.bd>
2020-10-30 10:23:25 +0600

    celery beat

--- src/food/celery.py
+++ src/food/celery.py
@@ -12,3 +12,13 @@ app = Celery('food_app')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 
 app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
+
+
+@app.on_after_configure.connect
+def setup_periodic_task(sender, **kwargs):
+    sender.add_periodic_task(10.0, test.s('hello'), name='add every 10')
+
+
+@app.task
+def test(value: str):
+    print(value)
